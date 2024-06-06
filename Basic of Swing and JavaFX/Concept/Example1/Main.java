48de757e69d521a1b2c6e49fc80685cbf187329c
abiralgtm <abiralgautam19@gmail.com>
2024-06-06 15:09:34 +0545

    Update Main.java

--- Basic of Swing and JavaFX/Concept/Example1/Main.java
+++ Basic of Swing and JavaFX/Concept/Example1/Main.java
@@ -1,14 +1,15 @@
 import javax.swing.*;
-class  Main
+
+class Main
 {
     public static void main(String[] args)
     {
         JFrame jFrame = new JFrame();
-
-        jFrame.setSize(400,400);
-        jFrame.setTitle("Hello world");
+        
         JLabel label1 = new JLabel("Hello world");
         jFrame.add(label1);
+        jFrame.setSize(400,400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setVisible(true);
     }
 }
